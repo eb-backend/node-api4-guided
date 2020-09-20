a945a0a7b94f4afaa651c68195bc6520379e7008
ebisLab <ebaiden@techie.com>
2020-09-20 09:47:49 -0400

    async example

--- async.js
+++ async.js
@@ -1,3 +1,6 @@
+const router = require("./api/api-router")
+const shoutsModel = require("./shouts/shouts-model")
+
 function greet(){
     return "Hello World"
 }
@@ -12,3 +15,41 @@ async function gree(){
 }
 
 gree() //A promixe with <Hello World>
+
+
+//example 2
+
+
+
+router.get("/", async(req,res,next)=>{
+    //*ideal
+    try{
+        const data = await shoutsModel.find()
+        const res1 = await someotherpromise()
+        const res2 = await someotherpromise()
+        const res3 = await someotherpromise()
+
+        res.status(200).json(data)
+    }
+    catch(err){
+        next(err)
+    }
+
+    //!not ideal
+shoutsModel.find()
+.then((res)=>{
+    return someotherpromise()
+})
+.then((res)=>{
+    return someotherpromise()
+})
+.then((res)=>{
+    return someotherpromise()
+})
+.catch(()=>{
+    
+})
+})
+
+
+
